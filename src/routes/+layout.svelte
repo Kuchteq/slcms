d69994691d415f81e4261294565f44969359c9dd
Kuchteq <mariusz.kuchta@triona.fi>
2024-04-01 16:01:56 +0200

    Initial view for some cms

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,56 @@
+<script>
+        import {
+                Header,
+                HeaderNav,
+                HeaderNavItem,
+                HeaderNavMenu,
+                SideNav,
+                SideNavItems,
+                SideNavMenu,
+                SideNavMenuItem,
+                SideNavLink,
+                SideNavDivider,
+                SkipToContent,
+                Content,
+                Grid,
+                Row,
+                Column,
+                Tag,
+        } from "carbon-components-svelte";
+        import {
+                WatsonHealthStackedScrolling_1,
+                WatsonHealthStackedScrolling_2,
+        } from "carbon-icons-svelte";
+
+        let isSideNavOpen = false;
+</script>
+
+<Header company="Kocia Kuchnia" platformName="SLCMS" bind:isSideNavOpen>
+        <svelte:fragment slot="skip-to-content">
+                <SkipToContent />
+        </svelte:fragment>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen}>
+        <SideNavItems>
+                <Tag icon={WatsonHealthStackedScrolling_1} size="sm">Pages</Tag>
+                <SideNavLink text="Landing Page" />
+                <SideNavLink text="About Us" />
+                <SideNavLink text="Recipes" />
+                <!-- <SideNavMenu text="Menu"> 
+                         <SideNavMenuItem href="/" text="Link 1" /> 
+                         <SideNavMenuItem href="/" text="Link 2" /> 
+                         <SideNavMenuItem href="/" text="Link 3" /> 
+                </SideNavMenu> -->
+                <SideNavDivider />
+                <Tag icon={WatsonHealthStackedScrolling_2} size="sm">
+                        Collections</Tag
+                >
+                <SideNavLink text="Blog Posts" />
+                <SideNavLink text="Products" />
+        </SideNavItems>
+</SideNav>
+
+<Content>
+        <slot/>
+</Content>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+        import {
+                Header,
+                HeaderNav,
+                HeaderNavItem,
+                HeaderNavMenu,
+                SideNav,
+                SideNavItems,
+                SideNavMenu,
+                SideNavMenuItem,
+                SideNavLink,
+                SideNavDivider,
+                SkipToContent,
+                Content,
+                Grid,
+                Row,
+                Column,
+                Tag,
+        } from "carbon-components-svelte";
+</script>
+
+
+        <Grid>
+                <Row>
+                        <h1>Welcome</h1>
+                </Row>
+                <Row>
+                        <Column noGutter>
+                                <h3>To jest h3</h3>
+                                <p>I piszemy cos normalnego</p>
+                                </Column>
+                </Row>
+        </Grid>
